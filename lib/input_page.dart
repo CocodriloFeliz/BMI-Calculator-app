317b5da7e6a6621e183c8524e59b4d504488a8cf
Amilkar Massy <massyamilkar@gmail.com>
2022-07-13 22:47:41 -0400

    Card eskeleton added

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -13,10 +13,58 @@ class _InputPageState extends State<InputPage> {
     return Scaffold(
       appBar: AppBar(
         title: const Text('BMI Calculator'),
+        centerTitle: true,
       ),
-      body: const Center(
-        child: Text('Body Text'),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: Row(
+                children: const [
+                  Expanded(
+                    child: ReusableCard(color: Color(0xFF323244)),
+                  ),
+                  Expanded(
+                    child: ReusableCard(color: Color(0xFF323244)),
+                  ),
+                ],
+              ),
+            ),
+            const Expanded(
+              child: ReusableCard(color: Color(0xFF323244)),
+            ),
+            Expanded(
+              child: Row(
+                children: const [
+                  Expanded(
+                    child: ReusableCard(color: Color(0xFF323244)),
+                  ),
+                  Expanded(
+                    child: ReusableCard(color: Color(0xFF323244)),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
+    );
+  }
+}
+
+class ReusableCard extends StatelessWidget {
+
+  final Color color;
+
+  const ReusableCard({Key? key, required this.color}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(10.0),
+      decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(10.0)),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -13,9 +13,8 @@ class BMICalculator extends StatelessWidget {
     return MaterialApp(
       home: const InputPage(),
       theme: ThemeData.dark().copyWith(
-        
-          scaffoldBackgroundColor: const Color(0xFF1D2136),
-          appBarTheme: const AppBarTheme(
+        scaffoldBackgroundColor: const Color(0xFF1D2136),
+        appBarTheme: const AppBarTheme(
             backgroundColor: Color(0xFF1D2136),
             shadowColor: Color.fromARGB(255, 15, 17, 27),
           )),
