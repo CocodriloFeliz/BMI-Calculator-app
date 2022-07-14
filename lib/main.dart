5df462a9849a463948297ecdd1edc13a2698ea68
Amilkar Massy <massyamilkar@gmail.com>
2022-07-13 20:41:30 -0400

    theme added

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'input_page.dart';
+
+void main() {
+  runApp(const BMICalculator());
+}
+
+class BMICalculator extends StatelessWidget {
+  const BMICalculator({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const InputPage(),
+      theme: ThemeData.dark().copyWith(
+        
+          scaffoldBackgroundColor: const Color(0xFF1D2136),
+          appBarTheme: const AppBarTheme(
+            backgroundColor: Color(0xFF1D2136),
+            shadowColor: Color.fromARGB(255, 15, 17, 27),
+          )),
+      );
+  }
+}
+
+
