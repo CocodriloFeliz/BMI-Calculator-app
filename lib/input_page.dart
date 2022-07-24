c0f1c11239f9889146c16a8f340c19132860783a
Amilkar Massy <massyamilkar@gmail.com>
2022-07-24 15:05:49 -0400

    buttons male and female added

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
-  
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 Color colorCard = const Color(0xFF323244);
 Color colorBottomContainer = const Color(0xFFE83D66);
+Color colorTitle = const Color(0xFF838491);
 const double bottomContainerHeight = 80.0;
 
 class InputPage extends StatefulWidget {
@@ -13,7 +14,6 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,10 +28,24 @@ class _InputPageState extends State<InputPage> {
               child: Row(
                 children: [
                   Expanded(
-                    child: ReusableCard(color: colorCard),
+                    child: ReusableCard(
+                      color: colorCard,
+                      child: ChildInfo(
+                        icon: const FaIcon(FontAwesomeIcons.mars),
+                        title: 'MALE',
+                        colorTitle: colorTitle,
+                      ),
+                    ),
                   ),
                   Expanded(
-                    child: ReusableCard(color: colorCard),
+                    child: ReusableCard(
+                      color: colorCard,
+                      child: ChildInfo(
+                        icon: const FaIcon(FontAwesomeIcons.venus),
+                        title: 'FEMALE',
+                        colorTitle: colorTitle,
+                      ),
+                      ),
                   ),
                 ],
               ),
@@ -64,19 +78,48 @@ class _InputPageState extends State<InputPage> {
   }
 }
 
+class ChildInfo extends StatelessWidget {
+  final FaIcon icon;
+  final String title;
+  final Color colorTitle;
+
+  const ChildInfo({Key? key, required this.icon, required this.title, required this.colorTitle}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        IconButton(
+          icon: icon,
+          onPressed: () {},
+        ),
+        const SizedBox(
+          height: 8.0,
+        ),
+        Text(
+          title,
+          style: TextStyle(color: colorTitle),
+        )
+      ],
+    );
+  }
+}
+
 class ReusableCard extends StatelessWidget {
-
   final Color color;
+  final Widget? child;
 
-  const ReusableCard({Key? key, required this.color}) : super(key: key);
+  const ReusableCard({Key? key, required this.color, this.child})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: const EdgeInsets.all(10.0),
       decoration: BoxDecoration(
-          color: color,
-          borderRadius: BorderRadius.circular(10.0)),
+          color: color, borderRadius: BorderRadius.circular(10.0)),
+      child: child,
     );
   }
 }
